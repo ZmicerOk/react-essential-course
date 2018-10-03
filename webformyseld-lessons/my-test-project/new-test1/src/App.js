a39a9ba91430505117c1b559807d0c5fd5043c00
Vasilkou Dzmitry <vasilkou@RFERL_BEL.local>
2018-10-03 18:03:51 +0300

    block2-less1

--- webformyseld-lessons/my-test-project/new-test1/src/App.js
+++ webformyseld-lessons/my-test-project/new-test1/src/App.js
@@ -4,24 +4,39 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //   <h1>hej!</h1>
+    //   <h2>you</h2>
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+    // );
+    return React.createElement(
+      'div',
+      {
+        className: "App"
+      },
+      React.createElement(
+        'h1',
+        null,
+        'Hello World!'
+      )
+      
+
+    )
   }
 }
 
