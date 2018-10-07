39cfd3cc34c07fb6a3408df847707574e117167d
ZmicerOk <vasilkou@73gmail.com>
2018-10-07 23:31:59 +0300

    some

--- webformyseld-lessons/my-test-project/new-test1/src/Car/Car.js
+++ webformyseld-lessons/my-test-project/new-test1/src/Car/Car.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default props => (
-    <section>
+    <section style={{
+        border: '1px solid #666666',
+        marginBottom: '10px'
+    }}>
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        <button onClick = {props.onChangeTitle}>Click</button>
+        <input type='text' onChange={props.onChangeTitle}/>
+        {/* <button onClick = {props.onChangeTitle}>Click</button> */}
         {/* This is <strong>Car</strong> Component! */}
     </section>
     )
