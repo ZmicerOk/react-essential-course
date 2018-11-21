9537ff27406f70f52bcd251244f6850a0a4c415b
ZmicerOk <vasilkou@73gmail.com>
2018-11-22 01:57:15 +0300

    app

--- webformyseld-lessons/react-theory/todo/src/index.js
+++ webformyseld-lessons/react-theory/todo/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder = "search"/>
+import React from "react";
+import ReactDOM from "react-dom";
+const TodoList = () => {
+  return (
     <ul>
-    <li>Learn React</li>
-    <li>Build Awesome App</li>
-    <li>Drink Tea</li>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+      <li>Drink Tea</li>
     </ul>
+  );
+};
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+const App = () =>{
+return(
+  <div>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList />
   </div>
 );
-ReactDOM.render(el,document.getElementById('root'));
+};
+
+ReactDOM.render(<App/>, document.getElementById("root"));
