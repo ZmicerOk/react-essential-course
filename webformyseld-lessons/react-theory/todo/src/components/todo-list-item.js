cd44713aeb19305503618237250b6d86eb4bc0b5
ZmicerOk <vasilkou@73gmail.com>
2018-11-26 23:13:44 +0300

    done + important

--- webformyseld-lessons/react-theory/todo/src/components/todo-list-item.js
+++ webformyseld-lessons/react-theory/todo/src/components/todo-list-item.js
@@ -2,22 +2,56 @@ import React, { Component } from "react";
 
 import "./todo-list-item.css";
 export default class TodoListItem extends Component {
+  state = {
+    done: false,
+    important: false
+  };
+
+  onLabelClick = () => {
+    this.setState(({done}) => {
+      return{
+      done: !done 
+      }
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState(({important}) => {
+      return {
+        important: !important
+      };
+    });
+  };
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     done: false
+  //   };
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${this.props.label}`);
+  //   };
+  // }
   render() {
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal"
-    };
+    const { label } = this.props; //destructuring
+    const { done, important } = this.state; //destructuring
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+    if (important) {
+      classNames += " important";
+    }
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" style={style}>
+      <span className={classNames}>
+        <span className="todo-list-item-label"
+         onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
-        >
+          onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
 
