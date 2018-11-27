341abba0c5ea8d6c79602bfb190846b7c78fddf5
ZmicerOk <vasilkou@73gmail.com>
2018-11-28 00:54:11 +0300

    to do was done!

--- webformyseld-lessons/react-theory/todo/src/components/app.js
+++ webformyseld-lessons/react-theory/todo/src/components/app.js
@@ -11,13 +11,14 @@ import "./app.css";
 export default class App extends Component {
   maxId = 100;
 
-  state = {
+  state = { 
     todoData: [
       this.createTodoItem("Drink Coffee"),
       this.createTodoItem("Make React App"),
       this.createTodoItem("Have a break")
     ], 
-    term: ''
+    term: '',
+    filter: 'all'//active, all, done
   };
 
   createTodoItem(label){
@@ -94,15 +95,29 @@ export default class App extends Component {
       return items;
     }
     return items.filter((item)=>{
-      return item.label.indexOf(term)>-1;
+      return item.label
+      .toLowerCase()
+      .indexOf(term.toLowerCase())>-1;
     });
   }
 
+  onFilterChange = (filter) =>{
+    this.setState({filter});
+  };
 
+filter (items, filter){
+  switch(filter){
+    case 'all': return items;
+    case 'active': return items.filter((item)=>!item.done);
+    case 'done': return items.filter((item)=>item.done);
+    default: return items;
+  }
+};
 
   render() {
-    const {todoData, term}=this.state;
-    const visibleItems = this.search(todoData, term);
+    const {todoData, term, filter}=this.state;
+    const visibleItems = this.filter(
+      this.search(todoData, term), filter);
     const doneCount = todoData.filter((el)=>el.done).length;
     const todoCount = todoData.length-doneCount;
     return (
@@ -110,7 +125,8 @@ export default class App extends Component {
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel onSearchChange = {this.onSearchChange}/>
-          <ItemStatusFilter />
+          <ItemStatusFilter filter = {filter}
+          onFilterChange={this.onFilterChange}/>
         </div>
         <TodoList
           todos={visibleItems}
