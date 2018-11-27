ec3ea30cffd4ab824bbe195d2d6a92867df9cc73
Vasilkou Dzmitry <vasilkou@RFERL_BEL.local>
2018-11-27 15:10:24 +0300

    onLabelChange -

--- webformyseld-lessons/react-theory/todo/src/components/item-add-form.js
+++ webformyseld-lessons/react-theory/todo/src/components/item-add-form.js
@@ -3,14 +3,21 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+    onLabelChange = () => {
+        console.log("*");
+    }
     render() {
         return (
-            <div className="item-add-form">
+            <form className="item-add-form d-flex">
+            <input  type= "text" 
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder = "What needs to bee done"/>
                 <button className="btn btn-outline-secondary"
                     onClick={() => this.props.onItemAdded("Hello!")}>
                     Add Item
-              </button>
-            </div>
+                </button>
+            </form>
         )
     }
 }
