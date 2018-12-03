5975ad3f0543da4ffc568d977cb37ea4475119e5
Vasilkou Dzmitry <vasilkou@RFERL_BEL.local>
2018-12-03 16:44:02 +0300

    icon+ onError

--- webformyseld-lessons/react-theory/star-db/src/components/app/app.js
+++ webformyseld-lessons/react-theory/star-db/src/components/app/app.js
@@ -6,12 +6,15 @@ import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator/error-indicator';
 
 export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: 5
+    selectedPerson: 5,
+    hasError: false
   };
 
   toggleRandomPlanet = () => {
@@ -28,8 +31,15 @@ export default class App extends Component {
     });
   };
 
-  render() {
+  componentDidCatch(){
+    console.log("componentDidCatch()");
+    this.setState({ hasError:true });
+  };
 
+  render() {
+if (this.state.hasError){
+  return <ErrorIndicator/>
+};
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet/> :
       null;
@@ -43,7 +53,7 @@ export default class App extends Component {
           onClick={this.toggleRandomPlanet}>
           Toggle Random Planet
         </button>
-
+<ErrorButton/>
         <div className="row mb2">
           <div className="col-md-6">
             <ItemList onItemSelected={this.onPersonSelected}/>
--- webformyseld-lessons/react-theory/star-db/src/components/error-indicator/error-indicator.js
+++ webformyseld-lessons/react-theory/star-db/src/components/error-indicator/error-indicator.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 import './error-indicator.css';
+import icon from './death-star.png';
 
 const ErrorIndicator = () =>{
     return(
         <div className="error-indicator">
+        <img src = {icon} alt="error icon"/>
             <span className="boom">Boom!</span>
             <p>something has gone terribly wrong</p>
             <p>(but we always sent droids to fix it)</p>
